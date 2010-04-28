40a3f44e81da0fca47abe7563984c3e33a2c9fcc
Tom Neudoerffer <tom.neudoerffer@gmail.com>
2010-04-28 18:00:58 -0400

    more rule ignores

--- pycheckers.py
+++ pycheckers.py
@@ -6,5 +6,5 @@ fn = os.path.join(os.getcwd(), sys.argv[1])
 
 print fn
 
-call(["pep8", "--ignore=E701", "--repeat", fn])
-call(["epylint.bat", fn], stderr=PIPE)
+call(["pep8", "--ignore=E701,E302", "--repeat", fn])
+call(["epylint.bat", "--disable-msg=W0511", fn], stderr=PIPE)
